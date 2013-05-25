c243dfc5e1144a5dde16e4e320121b43a0fc1c34
David Arbuckle <dintlu@gmail.com>
2013-05-25 12:56:54 -0600

    run.go now accepts a "settings" struct, defined in the file, which provides a means for the client application to set its configuration without requiring any modifications to the gofw backend.
    To reduce configuration, the client settings object is created by calling
    the public function NewSettings(), returning a pointer to a settings object
    which is then passed into Run().
    
    Run() will then bootstrap individual settings into the backend as needed.
    
    Signed-off-by: David Arbuckle <dintlu@gmail.com>

--- run.go
+++ run.go
@@ -8,19 +8,83 @@ import (
 
 
 var routes = Routes{}
+//var middleware = PackageMiddleware{}
 
-func RegisterRoutes(r *Routes) {
+
+type Template string //TEMP
+type DatabaseConfig struct {
+    Engine, Name, User, Password, Host, Port string
+}
+
+type Settings struct {
+    // Various boolean settings
+    Debug, I18N, L10N bool
+
+    // Administrator information
+    Admins [][2]string
+
+    // IP Address whitelist for debug output
+    InternalIP []string
+
+    // Database info
+    Databases map[string]DatabaseConfig
+
+    // Middleware handling functions
+    MiddlewareHandlers []Middleware
+
+    // Template processors (should there be more than one??)
+    TemplateProcessors []Template
+
+    // Project handler func for Routes
+    URLHandler func() Routes
+
+    // Hostnames from which requests will be permitted
+    AllowedHosts []string
+
+    // Public content settings - directories to expose to public, hostname from which to serve public requests.
+    PublicDirs []string
+    PublicHost string
+
+    // Time Zone
+    TimeZone string
+    Lang string
+
+    // Server configuration settings
+    BasePath string
+    Port string
+}
+
+//TODO:  create an initialization function that a client app can use to export a settings object.
+func NewSettings() *Settings {
+    return &Settings{
+        BasePath: "/", 
+        Port: "8081"}
+}
+
+
+func registerRoutes(r Routes) {
 	for _, url := range(r.routes) {
 		routes.routes = append(routes.routes, url)
 	}
 }
 
-func Run() {
+func Run(settings *Settings) {
 	// Run.go needs to expose an interface for a management script in a FW project to interact with.
 	// The FW project file will read the settings file, extract URLs and other information, register
     // functionality within the application, and finally process the request.
 	
 
-	http.HandleFunc("/", routes.baseHandler)
-	http.ListenAndServe(":8081", nil)
+
+
+    // Registering URLs declared in project base handler.
+    registerRoutes(settings.URLHandler())
+
+    
+
+    // Starting webserver
+	http.HandleFunc(settings.BasePath, routes.baseHandler)
+	http.ListenAndServe(":" + settings.Port, nil)
 }
+
+
+
